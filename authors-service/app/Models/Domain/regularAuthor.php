72d0cca554fea9c342ba906a69f8bdbacb306d1b
Betty Rodriguez <blrodriguez3@espe.edu.ec>
2026-02-01 13:35:21 -0500

    Update regularAuthor.php

--- authors-service/app/Models/Domain/regularAuthor.php
+++ authors-service/app/Models/Domain/regularAuthor.php
@@ -5,8 +5,8 @@ class RegularAuthor extends BaseAuthor {
     private $affiliation;
     private $registrationDate;
 
-    public function __construct($id, $name, $email, $bio, $affiliation, $registrationDate) {
-        parent::__construct($id, $name, $email, $bio);
+    public function __construct($idAuthor, $name, $email, $bio, $affiliation, $registrationDate) {
+        parent::__construct($idAuthor, $name, $email, $bio);
         $this->affiliation = $affiliation;
         $this->registrationDate = $registrationDate;
     }
