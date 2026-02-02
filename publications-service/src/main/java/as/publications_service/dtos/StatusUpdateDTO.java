5ae8240f375690bd7a3da149e3be4fb39f68257f
Betty Rodriguez <blrodriguez3@espe.edu.ec>
2026-02-01 20:19:22 -0500

    Revert "publicaciones"
    This reverts commit 477745be5ee5039eced102103dd22fcbb885476e.

--- publications-service/src/main/java/as/publications_service/dtos/StatusUpdateDTO.java
+++ publications-service/src/main/java/as/publications_service/dtos/StatusUpdateDTO.java
@@ -1,13 +1,8 @@
 package as.publications_service.dtos;
 
-import as.publications_service.entities.editorialStatus;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class StatusUpdateDTO {
-    @NotNull
-    @JsonProperty("status")
-    private editorialStatus newStatus;
+    private String status;
 }
--- publications-service/src/main/java/as/publications_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package as.publications_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
